d8c66be76f30cd5e6011beb860f397e457e89bc7
AmineBakhtaoui24 <amine.bakhtaoui97@gmail.com>
2020-01-31 15:10:48 +0100

    add produit

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Agent;
+use App\Entity\Produit;
+use JMS\Serializer\SerializerBuilder;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/produit", name="produit")
+     */
+    public function index()
+    {
+        $productRepository = $this->getDoctrine()->getManager()
+            ->getRepository(Produit::class);
+        $product = $productRepository->findAll();
+        $serializer = SerializerBuilder::create()->build();
+        $JMSproduct=$serializer->serialize($product, 'json');
+        $response = new Response($JMSproduct);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+}
